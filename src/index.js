93dcc696fa20cf3c15ebb5a1e37e85d61927743d
Leonardo Modena <leonardo.modena@marconirovereto.it>
2020-07-28 22:54:41 +0200

    v. 0.0.1 (drower designed)

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 import _ from 'lodash';
 import './style.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 import Logo from './img/logo.png';
 import { resolve } from 'url';
 const $ = require("jquery");
 
 let MAPS_KEY = process.env.BING_MAPS_API_KEY;
+let CLIMACELL_KEY = process.env.CLIMACELL_API_KEY;
 
 
 /******************************************************************************************************************************************************************************/
 //location prototype.
 class Location {
-    constructor(name, lat, long) {
+    constructor(id, name, lat, lon) {
+        this.id = id;
         this.name = name;
         this.point = {
-            lat,
-            long
+            lon,
+            lat
         }
     }
 }
 
 /******************************************************************************************************************************************************************************/
+
 
 
 //dynamic loader (credit: https://code-boxx.com/dynamically-load-javascript-css/)
@@ -59,22 +63,24 @@ var loader = {
 /*************************************************************************************************************************************************************************/
 //VARIABLES
 
+//boolean if geolocation is active
+let geolocation
+
 //the actual location
 let actualLocation;
-
-//location defined by geolocation
-let Geolocation;
 
 //boolean of the state of the search
 let searchOpenClose = false
 
+//interval
+let i;
 
 /*************************************************************************************************************************************************************************/
 
 $(".search-box").click(() => {
     searchOpenClose ? $("#search-input").removeClass("search-input-open") : $("#search-input").addClass("search-input-open")
     searchOpenClose ? $(".search-box").removeClass("move-search") : $(".search-box").addClass("move-search")
-    searchOpenClose = !searchOpenClose
+        //searchOpenClose = !searchOpenClose
 })
 
 /*************************************************************************************************************************************************************************/
@@ -82,18 +88,13 @@ $(".search-box").click(() => {
 
 
 //document ready callbacks
-$(function() {
+$(async function() {
 
-    console.log(Logo);
+    await start()
 
-    start()
+    loadMicrosoft()
 
-    //load bing map api script
-    loader.js(`https://www.bing.com/api/maps/mapcontrol?callback=GetMap&key=${MAPS_KEY}`)
-
-    getBackground();
-
-    setFirstLocation();
+    //setFirstLocation();
 
 });
 
@@ -101,10 +102,12 @@ $(function() {
 //set a default or a gelocated first location
 function setFirstLocation() {
     //try to get position by user
-    getPosition();
+    setTimeout(() => {
+        getPosition();
+    }, 2000);
+
     //set a default position
     setDefaultPosition();
-
 }
 
 
@@ -119,9 +122,9 @@ function getPosition() {
 
 
 function savePosition(position) {
-    setTimeout(() => {
-        setGeolocationLocation(position.coords.latitude, position.coords.longitude)
-    }, 200);
+    geolocation = true
+    setGeolocationLocation(position.coords.latitude, position.coords.longitude)
+    getWeather(actualLocation);
 }
 
 function showError(error) {
@@ -151,41 +154,45 @@ function showError(error) {
 
 // get default position and set it.
 async function setDefaultPosition() {
-    //let location = await getLocationClimacell("5f1575378bdd780012bcfec8")
-    console.log(location);
-    setMap(location.point.lat, location.point.lon)
+    actualLocation = await getLocationClimacell("5f1575378bdd780012bcfec8")
+    setMap(actualLocation.point.lat, actualLocation.point.lon)
+    getWeather(actualLocation)
 }
 
 //set the map and the name of the Geolocated location
 function setGeolocationLocation(lat, long) {
-    var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {});
+    var navigationBarMode = Microsoft.Maps.NavigationBarMode;
+    var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        navigationBarMode: navigationBarMode.square,
+        supportedMapTypes: [Microsoft.Maps.MapTypeId.road, Microsoft.Maps.MapTypeId.aerial]
+    });
+
     Microsoft.Maps.loadModule('Microsoft.Maps.Search', function() {
         var searchManager = new Microsoft.Maps.Search.SearchManager(map);
         var reverseGeocodeRequestOptions = {
+            location: new Microsoft.Maps.Location(parseFloat(lat), parseFloat(long)),
             callback: function(answer, userData) {
-                map.setView({
-                    center: new Microsoft.Maps.Location(Number.parseFloat(lat + ".0"), Number.parseFloat(long + ".0")),
-                    mapTypeId: Microsoft.Maps.MapTypeId.aerial,
-                });
+                actualLocation = new Location("geloaction", `${answer.address.locality}  ${answer.address.adminDistrict}`, lat, long)
+                map.setView({ bounds: answer.bestView });
                 map.entities.push(new Microsoft.Maps.Pushpin(reverseGeocodeRequestOptions.location));
-                Geolocation = new Location(`${answer.address.locality}  ${answer.address.adminDistrict}`, lat, long)
-                console.log(Geolocation);
-
+                getWeather(actualLocation);
             }
         };
         searchManager.reverseGeocode(reverseGeocodeRequestOptions);
     });
 
+
 }
 
 
 function setMap(lat, long) {
-    var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {});
-    map.setView({
-        center: new Microsoft.Maps.Location(lat, long),
-        mapTypeId: Microsoft.Maps.MapTypeId.aerial,
-        zoom: 8
+    var navigationBarMode = Microsoft.Maps.NavigationBarMode;
+    var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        navigationBarMode: navigationBarMode.square,
+        supportedMapTypes: [Microsoft.Maps.MapTypeId.road, Microsoft.Maps.MapTypeId.aerial]
     });
+    map.setView({ center: new Microsoft.Maps.Location(lat, long), mapTypeId: Microsoft.Maps.MapTypeId.aerial, zoom: 8 })
+
 
 }
 
@@ -229,27 +236,134 @@ const setB = (variable) => {
     console.log(variable)
 }
 
-const setLocation = (location) => {
-
-}
-
 
 
 function start() {
-    let logoIMG = document.getElementById("logo-big");
-    logoIMG.src = Logo;
+    return new Promise(resolve => {
+        getBackground();
+        let logoIMG = document.getElementById("logo-big");
+        logoIMG.src = Logo;
 
+        setTimeout(() => {
+            $("#logo-big").addClass("explodeLogo")
+        }, 800);
+
+        setTimeout(() => {
+            $(".first-page").addClass("displayApp");
+        }, 1080);
+
+        setTimeout(() => {
+            $(".initial-logo").hide();
+            $(".first-page").addClass("opacity");
+            $(".drower-bm").addClass("opacity");
+            resolve();
+        }, 1580);
+    });
+}
+
+
+function loadMicrosoft() {
     setTimeout(() => {
-        $("#logo-big").addClass("explodeLogo")
-    }, 800);
+        Microsoft.Maps.loadModule('Microsoft.Maps.AutoSuggest', {
+            credentials: `${MAPS_KEY}`,
+            callback: onLoad,
+            errorCallback: onError
+        });
+    }, 2000);
+}
 
-    setTimeout(() => {
-        $(".first-page").addClass("displayApp");
-    }, 1080);
+function onLoad() {
+    var options = { maxResults: 5 };
+    var manager = new Microsoft.Maps.AutosuggestManager(options);
+    manager.attachAutosuggest('#search-input', '#Sb', selectedSuggestion);
+}
 
-    setTimeout(() => {
-        $("#logo-big").hide();
-        $(".first-page").addClass("opacity");
-    }, 1580);
+function onError(message) {
+    console.log(message)
+    loadMicrosoft();
+}
 
+function selectedSuggestion(suggestionResult) {
+    console.log(suggestionResult.formattedSuggestion, suggestionResult.location.latitude, suggestionResult.location.longitude)
+}
+
+const getMinuteByMinute = (location) => {
+
+    async function myFetch() {
+        let response = await fetch(`https://api.climacell.co/v3/weather/realtime?lat=${location.point.lat}&lon=${location.point.lon}&unit_system=si&fields=temp%2Cfeels_like%2Cdewpoint%2Cprecipitation_type%2Cweather_code%2Csunrise%2Csunset%2Cprecipitation_probability%2Cprecipitation%2Cwind_speed%2Chumidity%2Cpm10&apikey=${CLIMACELL_KEY}`);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        } else {
+            let res = await response.json()
+            console.log(res)
+            setMinuteByMinute(res);
+
+        }
+    }
+    myFetch()
+        .catch(e => {
+            console.log('There has been a problem with your fetch operation: ' + e.message);
+        });
+}
+
+const getHours = (location) => {
+    let d = new Date();
+    let dateUTCnow = `${d.getUTCFullYear()}-${d.getUTCMonth()}-${d.getUTCDay()}T${d.getUTCHours()}%3A${d.getUTCMinutes()}%3A${d.getUTCSeconds()}Z`;
+    let dateUTCend = `${d.getUTCFullYear()}-${d.getUTCMonth()}-${d.getUTCDay()}T${d.getUTCHours() + 4}%3A${d.getUTCMinutes()}%3A${d.getUTCSeconds()}Z`;
+    async function myFetch() {
+        let response = await fetch(`https://api.climacell.co/v3/weather/forecast/hourly?lat=${location.point.lat}&lon=${location.point.lon}&unit_system=si&start_time=${dateUTCnow}&end_time=${dateUTCend}&apikey=CLJxtO5VC6ZdfLB4rB8I2YPQ5LSTtKsQ`);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        } else {
+            let res = await response.json()
+            console.log(res)
+            setMinuteByMinute(res);
+
+        }
+    }
+    myFetch()
+        .catch(e => {
+            console.log('There has been a problem with your fetch operation: ' + e.message);
+        });
+}
+
+const setMinuteByMinute = (data) => {
+
+    let deg = data.temp.value + "";
+
+    $(".deg").html(`${deg.substring(0, 2)}°`);
+
+    $(".deg-desc").html(getExpression(data.feels_like.value, data.precipitation_type.value));
+
+}
+
+function getExpression(percTmp, precp) {
+
+    let tempExp;
+
+    percTmp <= 7 ? tempExp = "cold" : null;
+    percTmp > 7 && percTmp <= 20 ? tempExp = "chilly" : null;
+    percTmp > 20 && percTmp <= 45 ? tempExp = "hot" : null;
+    percTmp < 0 ? tempExp = "cold" : null;
+    precp === "rain" ? tempExp = "rainy" : null;
+
+    return tempExp;
+
+}
+
+function getWeather(location) {
+    console.log(actualLocation)
+
+    clearInterval(i);
+
+    getMinuteByMinute(location);
+    i = setInterval(() => {
+        console.log(actualLocation)
+
+        getMinuteByMinute(location);
+    }, 60000);
+
+    getHours(location);
 }
